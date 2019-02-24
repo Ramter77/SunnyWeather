54e888355dd444237c4e8d1f4655c7cd4f64df79
JannisLauer <40301636+JannisLauer@users.noreply.github.com>
2019-02-24 01:13:44 +0100

    Basic moving Enemy
    First implementation of a simple AI that walks to predefined locations.

---
+++ Assets/BasicEnemy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicEnemy : MonoBehaviour
+{
+    [Header("Navigation")]
+    public int wanderIndex;
+    private NavMeshAgent agent;
+    private GameObject[] wander;
+    private Transform[] wanderPoints;
+
+    [Header("BehaviorStates")]
+    public bool attackEnabled = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        wander = GameObject.FindGameObjectsWithTag("WanderPoints");
+        wanderPoints = new Transform[wander.Length];
+        for (int i = 0; i < wander.Length; i++)
+        {
+            wanderPoints[i] = wander[i].transform;
+            //Debug.Log(wanderPoints[i]);
+        }
+        wanderIndex = Random.Range(0, wanderPoints.Length);
+        GoToNextPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDestinationReached();
+    }
+
+
+    public void GoToNextPoint()
+    {
+        agent.destination = wanderPoints[wanderIndex].position;
+    }
+
+    public void CheckDestinationReached()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 1f)
+        {
+            agent.isStopped = true;
+            attackEnabled = true;
+        }
+    }
+}
