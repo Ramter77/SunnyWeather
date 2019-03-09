e71329c921d2f9c3c0f088cc68ffda1827656c14
Ramter77 <34648210+Ramter77@users.noreply.github.com>
2019-03-09 15:53:59 +0100

    merge_Test

--- Assets/Scripts/Utility/AddComponentsFromEditor.cs
+++ Assets/Scripts/Utility/AddComponentsFromEditor.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(AddMaterialsToSelf))]
+[CustomEditor(typeof(AddComponentsFromScript))]
 [CanEditMultipleObjects]
 public class AddComponentsFromEditor : Editor
 {
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
+        
+        AddComponentsFromScript script = (AddComponentsFromScript)target;
 
-        AddMaterialsToSelf script = (AddMaterialsToSelf)target;
-
-        if (GUILayout.Button("Add Material to all selected GameObjects"))
+        if(GUILayout.Button("Add Material to all selected GameObjects"))
         {
             script.AddMaterialToSelectedGameObjects();
+        }
+
+        if(GUILayout.Button("Add Colliders to all selected GameObjects"))
+        {
+            script.AddCollidersToSelectedGameObjects();
+        }
+
+        if(GUILayout.Button("Add RigidBodies to all selected GameObjects"))
+        {
+            script.AddRigidBodiesToSelectedGameObjects();
         }
     }
 }
