8b9e1f97e1ea3e18a467db069418883fe6a2a6d1
KristiyanPetrov98 <39726172+KristiyanPetrov98@users.noreply.github.com>
2019-03-09 03:21:50 +0100

    ATLAS/Building/Building Fracture/Tower/Destroy Animation

--- Assets/Scripts/Utility/AddComponentsFromEditor.cs
+++ Assets/Scripts/Utility/AddComponentsFromEditor.cs
@@ -3,29 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(AddComponentsFromScript))]
+[CustomEditor(typeof(AddMaterialsToSelf))]
 [CanEditMultipleObjects]
 public class AddComponentsFromEditor : Editor
 {
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
-        
-        AddComponentsFromScript script = (AddComponentsFromScript)target;
 
-        if(GUILayout.Button("Add Material to all selected GameObjects"))
+        AddMaterialsToSelf script = (AddMaterialsToSelf)target;
+
+        if (GUILayout.Button("Add Material to all selected GameObjects"))
         {
             script.AddMaterialToSelectedGameObjects();
-        }
-
-        if(GUILayout.Button("Add Colliders to all selected GameObjects"))
-        {
-            script.AddCollidersToSelectedGameObjects();
-        }
-
-        if(GUILayout.Button("Add RigidBodies to all selected GameObjects"))
-        {
-            script.AddRigidBodiesToSelectedGameObjects();
         }
     }
 }
